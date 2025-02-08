f2dba436982654a1ef68d09e56c686e38bc5b803
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-09 04:11:37 +0500

    check for wrong entry
    if entry is wrong. it will print a message and recursively
    call itself until the user enters the correct answer

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ Repeat
  */
 // Create a user variable - userChoice
 function getUserChoice() {
-let userChoice = "";
-userChoice = prompt("Enter your move Rock,  Paper,  Scissor");
-return userChoice;
+    let userChoice = "";
+    userChoice = prompt("Enter your move Rock,  Paper,  Scissor");
+    if (userChoice == "Rock" || userChoice == "Paper" || userChoice == "Scissor") {
+        return userChoice;
+    }
+    else {
+        console.warn("Wrong Entry.\nTry Again");
+        return getUserChoice();
+    }
 }
 console.log(getUserChoice());
