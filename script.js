8351196efe85cdf4126484c73377bc7aa3c22d9d
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-09 03:57:11 +0500

    add the initial plan

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Plan = 
+Create a user variable - userChoice
+ask the user to enter their choice
+randomly generate a choice from Rock Paper Scissor
+decide the winner by checking against the game rule
+display the message and both choice
+Repeat
+ */
+// Create a user variable - userChoice
