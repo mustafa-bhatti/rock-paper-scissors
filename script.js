a14b172452377232f519c309b6bc0ff159baadbd
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-19 14:39:07 +0500

    add pixel font and change heading

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Plan = 
 Create a user variable - userChoice
-ask the user to enter their choice
+Get the userChoice from the button selected
 randomly generate a choice from Rock Paper Scissor
 decide the winner by checking against the game rule
 display the message and both choice
